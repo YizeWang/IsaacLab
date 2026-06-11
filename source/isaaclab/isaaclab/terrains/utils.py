681d84fd978075b2010e3d7b3944225916496c4f
Yize Wang <yizew@nvidia.com>
2026-06-11 17:27:41 +0800

    Comments
    Signed-off-by: Yize Wang <yizew@nvidia.com>

--- source/isaaclab/isaaclab/terrains/utils.py
+++ source/isaaclab/isaaclab/terrains/utils.py
@@ -52,6 +52,7 @@ def color_meshes_by_height(meshes: list[trimesh.Trimesh], **kwargs) -> trimesh.T
         # clip lower and upper bounds to have better color mapping
         heights_normalized = np.clip(heights_normalized, 0.1, 0.9)
         # Get the color for each vertex based on the height
+        # Valid options are ["viridis", "inferno", "plasma", "magma"]
         color_map = kwargs.pop("color_map", "inferno")
         colors = trimesh.visual.color.interpolate(heights_normalized, color_map=color_map)
         # Set the vertex colors
